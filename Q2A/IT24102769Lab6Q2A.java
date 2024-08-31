3bff91647e36d43a141b535793874312fad2a4f3
IT24102769 <it24102769@my.sliit.lk>
2024-08-31 12:50:22 +0530

    Add files via upload

---
+++ Q2A/IT24102769Lab6Q2A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class IT24102769Lab6Q2A
+{
+  public static void main(String[]args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int num=1;
+    while(num<=10)
+     {
+      System.out.print(num + " ");
+      num=num+1;
+     }
+   }
+}
+    
